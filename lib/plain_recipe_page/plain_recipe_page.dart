1ad866e65c20670aae52a67bbe77dcdfe96d7204
Gunhan Gulsoy <gunhangulsoy@gmail.com>
2021-08-08 23:42:52 -0700

    Plain recipe page: Moar scrollables

--- lib/plain_recipe_page/plain_recipe_page.dart
+++ lib/plain_recipe_page/plain_recipe_page.dart
@@ -6,46 +6,52 @@ class PlainRecipePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Plain Recipe Page')),
       body: Center(
-          child: Row(
-        children: [
-          // Placeholder for recipe menu
-          SizedBox(
-            width: 200,
-            height: 400,
-            child: ListView(
-              children: [
-                ListTile(title: Text("Butter Chicken")),
-                ListTile(title: Text("recipe 2"))
-              ],
-            ),
-          ),
-          VerticalDivider(),
-          SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: SizedBox(
-              width: 600,
-              height: 10000,
-              child: Card(
-                child: Column(
-                  children: [
-                    ListTile(
-                        title: Text('Dice Chicken'),
-                        subtitle: Text('foo bar baz')),
-                    Divider(),
-                    ListTile(
-                        title: Text(
-                            'Fry chicken with olive oil and tandoori masala'),
-                        subtitle: Text('foo bar baz')),
-                    Divider(),
-                    Text(
-                        'Some Recipe stuff\n\n\n\n\n\na\n\nn\n\n\n\na\n\n\n\n\na'),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      )),
+          child: SizedBox(
+              width: 820,
+              height: 800,
+              child: Row(
+                children: [
+                  // Placeholder for recipe menu
+                  SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: SizedBox(
+                      width: 200,
+                      height: 1000,
+                      child: ListView(
+                        children: [
+                          ListTile(title: Text("Butter Chicken")),
+                          ListTile(title: Text("recipe 2"))
+                        ],
+                      ),
+                    ),
+                  ),
+                  VerticalDivider(),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: SizedBox(
+                      width: 600,
+                      height: 10000,
+                      child: Card(
+                        child: Column(
+                          children: [
+                            ListTile(
+                                title: Text('Dice Chicken'),
+                                subtitle: Text('foo bar baz')),
+                            Divider(),
+                            ListTile(
+                                title: Text(
+                                    'Fry chicken with olive oil and tandoori masala'),
+                                subtitle: Text('foo bar baz')),
+                            Divider(),
+                            Text(
+                                'Some Recipe stuff\n\n\n\n\n\na\n\nn\n\n\n\na\n\n\n\n\na'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ))),
     );
   }
 }
