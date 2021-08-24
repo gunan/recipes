7e6d0ee4fcfa14ddbde5ca0f323c917d809e1d5a
Gunhan Gulsoy <gunhangulsoy@gmail.com>
2021-08-23 22:51:08 -0700

    Some search functionality! As inefficient as it can be!!!

--- lib/plain_recipe_page/recipe_list.dart
+++ lib/plain_recipe_page/recipe_list.dart
@@ -19,8 +19,28 @@ class _RecipeListState extends State<RecipeList> {
   final Function _callback;
 
   int _selectedRecipe = -1;
+  String _filterText = "";
+
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   _RecipeListState(this._recipeList, this._callback);
+
+  Map<int, Recipe> _createFilteredRecipeList(
+      List<Recipe> rList, String filter) {
+    Map<int, Recipe> retval = Map<int, Recipe>();
+    for (int i = 0; i < rList.length; i++) {
+      if (rList[i].name.contains(filter)) {
+        retval[i] = rList[i];
+      }
+    }
+    return retval;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,28 +55,33 @@ class _RecipeListState extends State<RecipeList> {
                   Icon(Icons.restaurant, color: Colors.grey, size: 40),
                   Text('Pick an item'),
                 ]),
-                TextField(),
+                TextField(
+                    controller: _controller,
+                    onChanged: (String value) async {
+                      setState(() {
+                        this._filterText = value;
+                      });
+                    }),
               ]))
             ];
-            int i = 0;
-            for (var rec in snapshot.data ?? []) {
-              int index = i;
+            _createFilteredRecipeList(snapshot.data ?? [], _filterText)
+                .forEach((key, value) {
               children.add(
                 GestureDetector(
                   child: Container(
-                    color: i == _selectedRecipe ? Colors.blue : Colors.white,
-                    child: ListTile(title: Text(rec.name)),
+                    color: key == _selectedRecipe ? Colors.blue : Colors.white,
+                    child: ListTile(title: Text(value.name)),
                   ),
                   onTap: () {
                     setState(() {
-                      this._selectedRecipe = index;
+                      this._selectedRecipe = key;
                     });
-                    this._callback(rec);
+                    this._callback(value);
                   },
                 ),
               );
-              i++;
-            }
+            });
+
             return ListView(children: children);
           } else if (snapshot.hasError) {
             return fancyErrorMessage(snapshot.error.toString());
